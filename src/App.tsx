a8f2231ac15b18c9791aae1110c7ae29d4e6e8d3
Jonathan <jonathandajome@gmail.com>
2025-02-13 19:37:40 -0500

    Add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+    </ThemeProvider>
   );
 };
 
